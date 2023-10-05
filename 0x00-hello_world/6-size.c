1a3dee94768ddc5b038b7f10635898f88552fb75
phil32w <philipkwetey02@gmail.com>
2023-10-05 10:57:33 -0700

    size of a bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %lu byte(s)", sizeof(char));
-	printf("size of int: %lu byt(s)", sizeof(int));
-	printf("size of long int: %lu byte(s)", sizeof(long int));
-	printf("size of long long int: %lu byte(s)", sizeof(long long int));
-	printf("size of a float: %lu byte(s)", sizeof(float));
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of int: %lu byt(s)\n", sizeof(int));
+	printf("size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
