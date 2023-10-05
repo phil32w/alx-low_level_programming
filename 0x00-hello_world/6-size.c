e90d76f7f61699c3feefbd21f6e9b80043a076c9
phil32w <philipkwetey02@gmail.com>
2023-10-05 15:35:10 -0700

    Size of a bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,20 +4,23 @@
 /**
  * main - main function
  *
- * Return: 0 Always success
+ * Return: 0 (Success)
  *
  */
 
 
 int main(void)
 {
-	printf(" Size of a char: %zu byte(s)\n ", sizeof(char));
-	printf(" Size of int: %zu byt(s)\n ", sizeof(int));
-	printf(" Size of long int: %zu byte(s)\n ", sizeof(long int));
-	printf("Size of long long int: %zu byte(s)\n ", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n ", sizeof(float));
-	return (0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of int: %lu byt(s)\n", (unsigned long)sizeof(b));
+printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
 }
-
-
-when
