85773e5818686a412b0739bc1280f30ec6db1b04
phil32w <philipkwetey02@gmail.com>
2023-10-05 11:52:23 -0700

    Size of a bitt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,17 @@
 /**
  * main - main function
  *
- * Return: 0 on positive outcome
+ * Return: 0 Always success
  *
  */
 
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of int: %lu byt(s)\n", sizeof(int));
-	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n ", sizeof(char));
+	printf("Size of int: %lu byt(s)\n ", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n ", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n ", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n ", sizeof(float));
 	return (0);
 }
